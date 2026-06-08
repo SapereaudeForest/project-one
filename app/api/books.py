ef85e30bad1027d6468a8e6171d0fe24821e0961
SapereaudeForest <22987864+SapereaudeForest@users.noreply.github.com>
2026-06-08 08:57:12 +0000

    Create POST API funcionality

--- app/api/books.py
+++ app/api/books.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'title' : 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'category': 'Fiction'},
+    {'title' : 'To Kill a Mockingbird', 'author': 'Harper Lee', 'category': 'Fiction'},
+    {'title' : '1984', 'author': 'George Orwell', 'category': 'Dystopian'},
+    {'title' : 'The Catcher in the Rye', 'author': 'J.D. Salinger', 'category': 'Fiction'},
+    {'title' : 'The Lord of the Rings', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'},
+    {'title' : 'Pride and Prejudice', 'author': 'Jane Austen', 'category': 'Romance'},
+    {'title' : 'The Hobbit', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'}
+]
+
 @app.get("/mybook")
 async def read_all_books():
-    return {"book title": "My favorite book"}
+    return BOOKS
 
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
@@ -30,18 +40,13 @@ async def read_author_books_category_by_query(book_author: str, category: str):
         return books_to_return
     return {"error": "Author or category book not found"}
 
-
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return {"message": "Book created successfully", "book": new_book}
 
 
           
 
-BOOKS = [
-    {'title' : 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'category': 'Fiction'},
-    {'title' : 'To Kill a Mockingbird', 'author': 'Harper Lee', 'category': 'Fiction'},
-    {'title' : '1984', 'author': 'George Orwell', 'category': 'Dystopian'},
-    {'title' : 'The Catcher in the Rye', 'author': 'J.D. Salinger', 'category': 'Fiction'},
-    {'title' : 'The Lord of the Rings', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'},
-    {'title' : 'Pride and Prejudice', 'author': 'Jane Austen', 'category': 'Romance'},
-    {'title' : 'The Hobbit', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'}
-]
+
 
