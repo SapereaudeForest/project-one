13e5605f88ab2d7fb3bac28ec47b70c06dde6d29
SapereaudeForest <22987864+SapereaudeForest@users.noreply.github.com>
2026-06-30 08:08:17 +0000

    Feature/put (#6)
    * create endpoint file
    
    * endpoints variants / update Readme
    
    * Read author category by querry
    
    * Create POST API funcionality
    
    * PUT and DETELE method

--- app/api/books.py
+++ app/api/books.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'title' : 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'category': 'Fiction'},
+    {'title' : 'To Kill a Mockingbird', 'author': 'Harper Lee', 'category': 'Fiction'},
+    {'title' : '1984', 'author': 'George Orwell', 'category': 'Dystopian'},
+    {'title' : 'The Catcher in the Rye', 'author': 'J.D. Salinger', 'category': 'Fiction'},
+    {'title' : 'The Lord of the Rings', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'},
+    {'title' : 'Pride and Prejudice', 'author': 'Jane Austen', 'category': 'Romance'},
+    {'title' : 'The Hobbit', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'}
+]
+
 @app.get("/mybook")
 async def read_all_books():
-    return {"book title": "My favorite book"}
+    return BOOKS
 
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
@@ -30,18 +40,27 @@ async def read_author_books_category_by_query(book_author: str, category: str):
         return books_to_return
     return {"error": "Author or category book not found"}
 
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return {"message": "Book created successfully", "book": new_book}
 
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for index in range(len(BOOKS)):
+        if BOOKS[index]['title'].lower() == updated_book.get('title').lower():
+            BOOKS[index] = updated_book
+            return {"message": "Book updated successfully", "book": updated_book}
 
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for index in range(len(BOOKS)):
+        if BOOKS[index].get('title').lower() == book_title.lower():
+            deleted_book = BOOKS.pop(index)
+            return {"message": "Book deleted successfully", "book": deleted_book}
+    return {"error": "Book not found"}
 
           
 
-BOOKS = [
-    {'title' : 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'category': 'Fiction'},
-    {'title' : 'To Kill a Mockingbird', 'author': 'Harper Lee', 'category': 'Fiction'},
-    {'title' : '1984', 'author': 'George Orwell', 'category': 'Dystopian'},
-    {'title' : 'The Catcher in the Rye', 'author': 'J.D. Salinger', 'category': 'Fiction'},
-    {'title' : 'The Lord of the Rings', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'},
-    {'title' : 'Pride and Prejudice', 'author': 'Jane Austen', 'category': 'Romance'},
-    {'title' : 'The Hobbit', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'}
-]
+
 
